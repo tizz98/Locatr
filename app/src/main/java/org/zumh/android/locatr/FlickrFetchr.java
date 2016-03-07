feb6e84de5e369c8bd20ce030096ecfb8b36df48
tizz98 <dev.tizz98@gmail.com>
2016-03-07 14:45:20 -0500

    init

---
+++ app/src/main/java/org/zumh/android/locatr/FlickrFetchr.java
@@ -0,0 +1,128 @@
+package org.zumh.android.locatr;
+
+import android.location.Location;
+import android.net.Uri;
+import android.util.Log;
+
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlickrFetchr {
+    private static final String TAG = "FlickrFetchr";
+
+    private static final String API_KEY = "016741516386fe8bb998bc8599367d4c";
+    private static final String FETCH_RECENTS_METHOD = "flickr.photos.getRecent";
+    private static final String SEARCH_METHOD = "flickr.photos.search";
+    private static final Uri ENDPOINT = Uri
+            .parse("https://api.flickr.com/services/rest")
+            .buildUpon()
+            .appendQueryParameter("api_key", API_KEY)
+            .appendQueryParameter("format", "json")
+            .appendQueryParameter("nojsoncallback", "1")
+            .appendQueryParameter("extras", "url_s")
+            .build();
+
+    public byte[] getUrlBytes(String urlSpec) throws IOException {
+        URL url = new URL(urlSpec);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            InputStream in = connection.getInputStream();
+
+            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                throw new IOException(connection.getResponseMessage() + ": with " + urlSpec);
+            }
+
+            int bytesRead;
+            byte[] buffer = new byte[1024];
+            while ((bytesRead = in.read(buffer)) > 0) {
+                out.write(buffer, 0, bytesRead);
+            }
+            out.close();
+            return out.toByteArray();
+        } finally {
+            connection.disconnect();
+        }
+    }
+
+    public String getUrlString(String urlSpec) throws IOException {
+        return new String(getUrlBytes(urlSpec));
+    }
+
+    private String buildUrl(String method, int page_number, String query) {
+        Uri.Builder uriBuilder = ENDPOINT.buildUpon()
+                .appendQueryParameter("method", method)
+                .appendQueryParameter("page", Integer.toString(page_number));
+
+        if (method.equals(SEARCH_METHOD)) {
+            uriBuilder.appendQueryParameter("text", query);
+        }
+
+        return uriBuilder.build().toString();
+    }
+
+    private String buildUrl(Location location) {
+        return ENDPOINT.buildUpon()
+                .appendQueryParameter("method", SEARCH_METHOD)
+                .appendQueryParameter("lat", "" + location.getLatitude())
+                .appendQueryParameter("lon", "" + location.getLongitude())
+                .build().toString();
+    }
+
+    public List<GalleryItem> fetchRecentPhotos(int page_number) {
+        String url = buildUrl(FETCH_RECENTS_METHOD, page_number, null);
+        return downloadGalleryItems(url);
+    }
+
+    public List<GalleryItem> searchPhotos(String query, int page_number) {
+        String url = buildUrl(SEARCH_METHOD, page_number, query);
+        return downloadGalleryItems(url);
+    }
+
+    public List<GalleryItem> searchPhotos(Location location) {
+        String url = buildUrl(location);
+        return downloadGalleryItems(url);
+    }
+
+    private List<GalleryItem> downloadGalleryItems(String url) {
+        List<GalleryItem> items = new ArrayList<>();
+
+        try {
+            String jsonString = getUrlString(url);
+            JSONObject jsonBody = new JSONObject(jsonString);
+            parseItems(items, jsonBody);
+        } catch (JSONException je) {
+            Log.e(TAG, "Failed to parse JSON", je);
+        } catch (IOException ioe) {
+            Log.e(TAG, "Failed to fetch items", ioe);
+        }
+
+        return items;
+    }
+
+    private void parseItems(List<GalleryItem> items, JSONObject jsonBody) throws IOException, JSONException {
+        JSONObject photosJsonObject = jsonBody.getJSONObject("photos");
+        JSONArray photosJsonArray = photosJsonObject.getJSONArray("photo");
+
+        Gson gson = new Gson();
+
+        for (int i = 0; i < photosJsonArray.length(); ++i) {
+            JSONObject photoJsonObject = photosJsonArray.getJSONObject(i);
+
+            GalleryItem item = gson.fromJson(photoJsonObject.toString(), GalleryItem.class);
+            items.add(item);
+        }
+    }
+}
---
+++ app/src/main/java/org/zumh/android/locatr/GalleryItem.java
@@ -0,0 +1,66 @@
+package org.zumh.android.locatr;
+
+import android.net.Uri;
+
+import com.google.gson.annotations.SerializedName;
+
+public class GalleryItem {
+    @SerializedName("title")
+    private String mCaption;
+
+    @SerializedName("id")
+    private String mId;
+
+    @SerializedName("url_s")
+    private String mUrl;
+
+    @SerializedName("owner")
+    private String mOwner;
+
+    @Override
+    public String toString() {
+        return mCaption;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
+
+    public String getId() {
+
+        return mId;
+    }
+
+    public void setId(String id) {
+        mId = id;
+    }
+
+    public String getCaption() {
+
+        return mCaption;
+    }
+
+    public void setCaption(String caption) {
+        mCaption = caption;
+    }
+
+    public String getOwner() {
+        return mOwner;
+    }
+
+    public void setOwner(String owner) {
+        mOwner = owner;
+    }
+
+    public Uri getPhotoPageUri() {
+        return Uri.parse("http://www.flickr.com/photos/")
+                .buildUpon()
+                .appendPath(mOwner)
+                .appendPath(mId)
+                .build();
+    }
+}
